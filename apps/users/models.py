82057d748dafdb75656b313db5d2ed47e68c4cfe
superm <masanping@aliyun.com>
2019-09-19 13:41:14 +0800

    xadmin 相关文件

--- apps/users/models.py
+++ apps/users/models.py
@@ -11,7 +11,7 @@ class UserProfile(AbstractUser):
     birthday = models.DateField(verbose_name='生日', null=True, blank=True)
     gender = models.CharField(verbose_name='性别', choices=Gender_choice, max_length=6)
     address = models.CharField(max_length=100, verbose_name='地址', default='')
-    mobile = models.CharField(max_length=11, unique=True, verbose_name='手机号码')
+    mobile = models.CharField(max_length=11, verbose_name='手机号码')
     image = models.ImageField(upload_to='head_image/%Y/%m', default='head_image/default.jpg', verbose_name='头像')
 
     class Meta:
